045e654be1c347e94947d8ccc5553d2786b62a28
test <zhasik.asik01@gmail.com>
2022-05-09 15:46:07 +0600

    fix: linaer gradient package

--- MyApp/src/screens/Wallet.tsx
+++ MyApp/src/screens/Wallet.tsx
@@ -1,15 +1,32 @@
 import { FC } from "react";
 import React from 'react'
-import {View} from "react-native";
+import {StyleSheet, View} from "react-native";
 import {Box} from "../components/UI/Box/Box";
 import {Text} from "react-native-paper";
+import LinearGradient from 'react-native-linear-gradient';
+
 
 export const WalletScreen: FC = () => {
-    return  <View>
-        <Box>
-            <Text>
-                Settings Page
-            </Text>
-        </Box>
-    </View>
+    return  <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
+        <Text style={styles.buttonText}>
+            Sign in with Facebook
+        </Text>
+    </LinearGradient>
 }
+
+const styles = StyleSheet.create({
+    linearGradient: {
+        flex: 1,
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 5
+    },
+    buttonText: {
+        fontSize: 18,
+        fontFamily: 'Gill Sans',
+        textAlign: 'center',
+        margin: 10,
+        color: '#ffffff',
+        backgroundColor: 'transparent',
+    },
+});
